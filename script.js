635295a1d880de8d7bbf62f0807fa08b487d74b5
jak-ing <lupin2702@gmail.com>
2017-06-04 00:33:56 +0200

    Make basic category filtering work

--- script.js
+++ script.js
@@ -18,12 +18,22 @@ angular.module('datasourcesApp', ["firebase"])
         $scope.sortType = 'name'; // set the default sort type
         $scope.sortReverse = false; // set the default sort order
         $scope.searchTerm = ''; // set the default search/filter term
-        $scope.categoryFilter = '';
+        // maybe use own small database that includes all the categories here instead?
+        $scope.categoryFilter = new Set(["Government", "Health", "Education", "Facts"]);
 
         $scope.showData = function(datasource) {
-            return datasource.category.includes($scope.categoryFilter) &&
+            return $scope.categoryFilter.has(datasource.category) &&
                 (datasource.name.includes($scope.searchTerm) ||
                     datasource.description.includes($scope.searchTerm) ||
-                    datasource.link.includes($scope.searchTerm))
+                    datasource.link.includes($scope.searchTerm));
+        };
+
+        $scope.toggleCategory = function($event, category) {
+            if (!$scope.categoryFilter.delete(category)) { // is true if category was not present in categoryFilter
+                $scope.categoryFilter.add(category);
+                $event.target.classList.remove('disabled');
+            } else {
+                $event.target.classList.add('disabled');
+            }
         };
     });
